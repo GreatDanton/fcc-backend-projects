7d3d5815b6f8a3fb17635b94ef23c4f902cd6f4a
GreatDanton <pribosek.jan@gmail.com>
2017-09-22 13:43:50 +0200

    Added register password error checks

--- Dynamic_web_applications/1.Voting_app/controllers/authentication.go
+++ Dynamic_web_applications/1.Voting_app/controllers/authentication.go
@@ -11,21 +11,66 @@ import (
 
 // Register is handling registration of Voting application
 func Register(w http.ResponseWriter, r *http.Request) {
-
-	if r.Method == "GET" {
-		t := template.Must(template.ParseFiles("templates/register.html", "templates/navbar.html", "templates/styles.html"))
-		err := t.Execute(w, nil)
-		if err != nil {
-			fmt.Println(err)
-			http.Error(w, "Internal server error", http.StatusInternalServerError)
-			return
-		}
-	} else if r.Method == "POST" {
-		fmt.Fprintf(w, "Posted data")
+	switch m := r.Method; m {
+	case "GET":
+		registerGET(w, r, registerErrors{})
+	case "POST":
+		registerNewUser(w, r)
+	default:
+		registerGET(w, r, registerErrors{})
 	}
 }
 
-// Login is handling login part
+type registerErrors struct {
+	Username      string
+	ErrorUsername string
+
+	Email      string
+	ErrorEmail string
+
+	Password      string
+	ErrorPassword string
+}
+
+// registerGET displays register template
+func registerGET(w http.ResponseWriter, r *http.Request, errors registerErrors) {
+	t := template.Must(template.ParseFiles("templates/register.html", "templates/navbar.html", "templates/styles.html"))
+	err := t.Execute(w, errors)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+}
+
+func registerNewUser(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	email := r.Form["email"][0]
+	pass := r.Form["password"][0]
+	passConfirm := r.Form["password-confirm"][0]
+	username := r.Form["register-name"][0]
+
+	// check password
+	// check email
+	//
+	// check if username or email exist in db
+
+	if pass != passConfirm {
+		errMsg := registerErrors{
+			Username:      username,
+			Email:         email,
+			Password:      pass,
+			ErrorPassword: "Passwords do not match",
+		}
+		registerGET(w, r, errMsg)
+		return
+	}
+
+	fmt.Println(username, email, pass, passConfirm)
+}
+
+// Login is handling login part + authentication
+// TODO:
 func Login(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method == "GET" {
